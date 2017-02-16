11cfdbff4f18804d19fe51087c9498eb22195ffd
Philip <phucvd.ce@gmail.com>
2017-02-16 10:44:45 +0700

    Update install.sh

--- engine/installation/ubuntu/16.04/install.sh
+++ engine/installation/ubuntu/16.04/install.sh
@@ -1,1 +1,23 @@
 #/bin/sh
+
+sudo apt-get update -y
+sudo apt-get install -y --no-install-recommends \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+sudo apt-get update -y
+sudo apt-get -y install docker-engine
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+docker run hello-world
+
+

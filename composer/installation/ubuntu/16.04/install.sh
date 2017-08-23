d4645b54e31d157cb15c5091e3dc2e4df41d5e9e
Philip <phucvd.ce@gmail.com>
2017-08-23 20:33:09 +0700

    Create install.sh

---
+++ composer/installation/ubuntu/16.04/install.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+sudo apt-get update -y
+sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose -v

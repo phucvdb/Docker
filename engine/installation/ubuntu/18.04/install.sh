a346095e733f818ce65a81e0eed41cfecc380d41
Philip <phucvd.ce@gmail.com>
2019-01-29 12:14:49 +0700

    auto setup

--- engine/installation/ubuntu/18.04/install.sh
+++ engine/installation/ubuntu/18.04/install.sh
@@ -8,7 +8,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
 sudo apt update -y
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 
 sudo usermod -aG docker ${USER}
 sudo systemctl status docker

946843d5e5496716df354d564733933945a1d61e
Philip <phucvd.ce@gmail.com>
2019-01-29 11:28:10 +0700

    Create install.sh

---
+++ engine/installation/ubuntu/18.04/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -X
+
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+sudo apt update -y
+sudo apt install docker-ce
+
+sudo usermod -aG docker ${USER}
+sudo systemctl status docker
